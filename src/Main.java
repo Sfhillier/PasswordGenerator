ff5064b3394a7e5d5f5cf9ab0e95c3c63b199f53
Scott Hillier <Sfhillier@gmail.com>
2016-03-29 22:50:55 -0400

    Added files via upload
    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author Scott Hillier
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		// Variables needed for password generator
+		
+	}
+
+}
